00a8f9d4a1e4fbe3eb88ca1ceda99b6846404576
Kemila Illankoon <zemikone@gmail.com>
2020-09-18 06:44:55 +1000

    addPersonality changeGpa shortlistProjects selectRoles selectUnlikeMem
    Done!

---
+++ Team Formation/src/ProjectManager.java
@@ -0,0 +1,247 @@
+import Model.Project;
+import Model.Student;
+import Model.Team;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProjectManager {
+
+     ArrayList<Project> projectsList;
+     ArrayList<Student> studentList;
+     ArrayList<Team> teamList;
+
+    public ProjectManager(ArrayList<Project> projectsList, ArrayList<Student> studentList, ArrayList<Team> teamList) {
+        this.projectsList = projectsList;
+        this.studentList = studentList;
+        this.teamList = teamList;
+    }
+
+
+
+    public void addPersonality() {
+        if (studentList.size() == 0) {
+            System.out.println("Please add Students File First!");
+        } else {
+
+            String personality = "";
+            String studentId = "";
+            String selectedStdId = "";
+            String cnflctStdId1 = "";
+            String cnflctStdId2 = "";
+
+
+            // select student
+            Boolean isIdMatched = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the ID of the Student from below list to capture t" +
+                        "he Student Personalities");
+                System.out.println("++++Students List++++");
+                System.out.println(" ID");
+                System.out.println(" --");
+                for (Student student : studentList) {
+                    System.out.println(" " + student.getId());
+                }
+                try {
+                    studentId = keyboard.nextLine();
+                    for (Student student : studentList) {
+                        if (student.getId().equals(studentId)) {
+                            isIdMatched = true;
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Student ID");
+                }
+                if (isIdMatched == false)
+                    System.out.println("Sorry, please enter Student ID in the Students List");
+            } while (isIdMatched == false);
+            selectedStdId = studentId;
+
+            //select Personality
+            Boolean isPrsnIdValid = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the Personality Type of the Student from below list of Characteristics");
+                System.out.println("++++Personality List++++");
+                System.out.println(" Personality Type");
+                System.out.println(" ----------------");
+                System.out.println(" A");
+                System.out.println(" B");
+                System.out.println(" C");
+                System.out.println(" D");
+                System.out.println(" E");
+                System.out.println(" F");
+
+
+                try {
+                    personality = keyboard.nextLine();
+                    if (personality.equals("A")
+                            || personality.equals("B")
+                            || personality.equals("C")
+                            || personality.equals("D")
+                            || personality.equals("E")
+                            || personality.equals("F")) {
+                            for(Student student: studentList){
+                                if(student.getId().equals(selectedStdId)){
+                                    student.setPersonality(personality);
+                                    isPrsnIdValid =true;
+                                    break;
+                                }
+                            }
+
+
+                    } else {
+                        System.out.println("Sorry, please enter valid Personality Type");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Personality Type");
+                }
+
+            } while (isPrsnIdValid == false);
+
+            TeamFormationMain.generateStudentsFile(studentList);
+        }
+
+
+    }
+
+    public void changeGpa() throws Exception {
+        if (studentList.size() == 0) {
+            System.out.println("Please add Students File First!");
+        } else {
+
+            String gpaValue = "";
+            String studentId = "";
+            String selectedStdId = "";
+
+
+            // select student
+            Boolean isIdMatched = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the ID of the Student from below list to change t" +
+                        "he GPA");
+                System.out.println("++++Students List++++");
+                System.out.println(" ID");
+                System.out.println(" --");
+                for (Student student : studentList) {
+                    System.out.println(" " + student.getId());
+                }
+                try {
+                    studentId = keyboard.nextLine();
+                    for (Student student : studentList) {
+                        if (student.getId().equals(studentId)) {
+                            isIdMatched = true;
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Student ID");
+                }
+                if (isIdMatched == false)
+                    System.out.println("Sorry, please enter Student ID in the Students List");
+            } while (isIdMatched == false);
+            selectedStdId = studentId;
+
+            //select Personality
+            Boolean isPrsnIdValid = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the GPA you want to assign.");
+                try {
+                    gpaValue = keyboard.nextLine();
+                    Double gpa =Double.parseDouble(gpaValue);
+
+                    if(gpa>4){
+                        System.out.println("Sorry, please enter valid GPA");
+                    }else{
+                        for(Student student: studentList){
+                            if(student.getId().equals(selectedStdId)){
+                                student.setGpa(gpa);
+                                isPrsnIdValid =true;
+                                break;
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Personality Type");
+                }
+
+            } while (isPrsnIdValid == false);
+
+            TeamFormationMain.generateStudentsFile(studentList);
+            TeamFormationMain.manageStudent();
+        }
+    }
+
+    public void shortlistProjects() throws Exception {
+        if (projectsList.size() >4) {
+            for (Project project : projectsList) {
+                String tempProjId = project.getpId();
+                for(Student student: studentList){
+                    for(String prefproj : student.getPrefProjects()){
+                        if(prefproj.equals(tempProjId)){
+                            project.setPrefCount(project.getPrefCount()+1);
+                        }
+                    }
+                }
+            }
+
+            //Sorting the projects
+            boolean sorted = false;
+            Project temp;
+            while (!sorted) {
+                sorted = true;
+                for (int i = 0; i < projectsList.size() - 1; i++) {
+                    if (projectsList.get(i).getPrefCount() < projectsList.get(i + 1).getPrefCount()) {
+                        temp = projectsList.get(i);
+                        projectsList.set(i, projectsList.get(i + 1));
+                        projectsList.set(i + 1, temp);
+                        sorted = false;
+                    }
+                }
+            }
+//            To shortlist projects based on no of students
+//            int noProjects = studentList.size()/4;
+
+            //Remove the rest of the projects keeping the most prefered ones
+            ArrayList<Project> tempProjectsList = new ArrayList();
+            for (int i = 0; i < 5; i++) {
+                tempProjectsList.add(projectsList.get(i));
+            }
+            projectsList = tempProjectsList;
+
+        }
+
+
+        System.out.println("++++Project List++++");
+        System.out.println(" ID     Project Title    Pref Count");
+        System.out.println(" --     -------------    ----------");
+        for (Project project : projectsList) {
+            System.out.println(" " + project.getpId() + "     " + project.getName() + "    " + project.getPrefCount());
+        }
+
+        System.out.println("Projects have been Shortlisted Successfully!");
+
+
+    }
+
+    public void assignStudents(){
+
+    }
+
+    public void changeConstraints(){
+
+    }
+
+    public void swapMembers(){
+
+    }
+
+
+
+
+
+}
--- Team Formation/src/StudentRep.java
+++ Team Formation/src/StudentRep.java
@@ -1,2 +1,226 @@
+import Model.Project;
+import Model.Student;
+import Model.Team;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentRep {
+
+    ArrayList<Project> projectsList;
+    ArrayList<Student> studentList;
+    ArrayList<Team> teamList;
+
+    public StudentRep(ArrayList<Project> projectsList, ArrayList<Student> studentList, ArrayList<Team> teamList) {
+        this.projectsList = projectsList;
+        this.studentList = studentList;
+        this.teamList = teamList;
+    }
+
+
+    public void selectPrefProjects(){
+
+    }
+
+    public void selectRoles() throws Exception {
+        if (studentList.size() == 0) {
+            System.out.println("Please add Students File First!");
+        } else {
+
+            String roleID = "";
+            String selectedStdId = "";
+            Student selectedStudent = null;
+
+
+            // select student
+            Boolean isIdMatched = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the ID of the Student you wish to add select Roles");
+                System.out.println("++++Students List++++");
+                System.out.println(" ID");
+                System.out.println(" --");
+                for (Student student : studentList) {
+                    System.out.println(" " + student.getId());
+                }
+                try {
+                    selectedStdId = keyboard.nextLine();
+                    for (Student student : studentList) {
+                        if (student.getId().equals(selectedStdId)) {
+                            isIdMatched = true;
+                            selectedStudent =student;
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Student ID");
+                }
+                if (isIdMatched == false)
+                    System.out.println("Sorry, please enter Student ID in the Students List");
+            } while (isIdMatched == false);
+
+
+            boolean isBack=false;
+            String yesOrNo = "";
+
+            do {
+                if(selectedStudent.getRoles().size()<2) {
+                    Scanner keyboard = new Scanner(System.in);
+                    Boolean isConfIdMatched = false;
+                    System.out.println("Do you want to add a Role for the Student " + selectedStdId);
+                    System.out.println(" Y - Yes");
+                    System.out.println(" N - No");
+                    yesOrNo = keyboard.nextLine();
+                    if (yesOrNo.equalsIgnoreCase("Y")) {
+                        yesOrNo = "";
+                        do {
+                            System.out.println("Please enter the ID of the Role you wish to add or enter B to go back.");
+                            System.out.println("++++Roles++++");
+                            System.out.println("ID Role");
+                            System.out.println("-- ----");
+                            System.out.println("R1.Programmer");
+                            System.out.println("R2.Database Administrator");
+                            System.out.println("R3.Leader");
+                            System.out.println("R4.UI Designer");
+
+                            try {
+                                roleID = keyboard.nextLine();
+                                if (!roleID.equalsIgnoreCase("b")) {
+                                    if (roleID.equals("R1")
+                                            || roleID.equals("R2")
+                                            || roleID.equals("R3")
+                                            || roleID.equals("R4")) {
+                                        isConfIdMatched = true;
+                                        selectedStudent.getRoles().add(roleID);
+                                        System.out.println("Student "+roleID+ " added as a Preferred Role");
+                                        TeamFormationMain.generateStudentsFile(studentList);
+                                    } else {
+                                        System.out.println("Sorry, please enter valid Role ID");
+                                    }
+
+                                } else {
+                                    isConfIdMatched = true;
+                                }
+                            } catch (Exception e) {
+                                System.out.println("Sorry, please enter valid Role ID");
+                            }
+                            if (isConfIdMatched == false)
+                                System.out.println("Sorry, please enter Role ID in the Roles List");
+                        } while (isConfIdMatched == false);
+                    } else if (yesOrNo.equalsIgnoreCase("N")) {
+                        isBack = true;
+
+                    } else {
+                        System.out.println("Sorry, please enter valid input Y or N");
+                    }
+
+                }else{
+                    System.out.println("This member has 2 Preferred Roles");
+                    TeamFormationMain.manageStudent();
+                }
+            } while (isBack == false);
+
+        }
+    }
+
+    public void selectUnlikeMem() throws Exception {
+        if (studentList.size() == 0) {
+            System.out.println("Please add Students File First!");
+        } else {
+
+            String personality = "";
+            String studentId = "";
+            String selectedStdId = "";
+            Student selectedStudent = null;
+
+
+            // select student
+            Boolean isIdMatched = false;
+            do {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter the ID of the Student you wish to add the Conflicted Students");
+                System.out.println("++++Students List++++");
+                System.out.println(" ID");
+                System.out.println(" --");
+                for (Student student : studentList) {
+                    System.out.println(" " + student.getId());
+                }
+                try {
+                    studentId = keyboard.nextLine();
+                    for (Student student : studentList) {
+                        if (student.getId().equals(studentId)) {
+                            isIdMatched = true;
+                            selectedStudent =student;
+                            break;
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Sorry, please enter valid Student ID");
+                }
+                if (isIdMatched == false)
+                    System.out.println("Sorry, please enter Student ID in the Students List");
+            } while (isIdMatched == false);
+            selectedStdId = studentId;
+
+            boolean isBack=false;
+            String yesOrNo = "";
+
+            do {
+                if(selectedStudent.getUnlikeMemb().size()<3) {
+                    Scanner keyboard = new Scanner(System.in);
+                    Boolean isConfIdMatched = false;
+                    System.out.println("Do you want to add a Conflicted student for Student " + selectedStdId);
+                    System.out.println(" Y - Yes");
+                    System.out.println(" N - No");
+                    yesOrNo = keyboard.nextLine();
+                    if (yesOrNo.equalsIgnoreCase("Y")) {
+                        yesOrNo = "";
+                        do {
+                            System.out.println("Please enter the ID of the Student you wish to add as a Conflicted Student of " + selectedStdId + " or enter B to go back.");
+                            System.out.println("++++Students List++++");
+                            System.out.println(" ID");
+                            System.out.println(" --");
+                            for (Student student : studentList) {
+                                System.out.println(" " + student.getId());
+                            }
+                            try {
+                                studentId = keyboard.nextLine();
+                                if (!studentId.equalsIgnoreCase("b")) {
+                                    for (int i = 0; i < studentList.size(); i++) {
+                                        if (studentList.get(i).getId().equals(studentId)) {
+                                            isConfIdMatched = true;
+                                            selectedStudent.getUnlikeMemb().add(studentId);
+                                            System.out.println("Student "+studentId+ " added as a Conflicted Student!");
+                                            TeamFormationMain.generateStudentsFile(studentList);
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    isConfIdMatched = true;
+//                                    isBack = true;
+                                }
+                            } catch (Exception e) {
+                                System.out.println("Sorry, please enter valid Student ID");
+                            }
+                            if (isConfIdMatched == false)
+                                System.out.println("Sorry, please enter Student ID in the Students List");
+                        } while (isConfIdMatched == false);
+                    } else if (yesOrNo.equalsIgnoreCase("N")) {
+                        isBack = true;
+
+                    } else {
+                        System.out.println("Sorry, please enter valid input Y or N");
+                    }
+
+                }else{
+                    System.out.println("This member has 3 Conflicted Students.");
+                    TeamFormationMain.manageStudent();
+                }
+            } while (isBack == false);
+
+        }
+
+
+    }
+
 }
